8f5d0776bc4730a174b92ebeeef756d5dcd2cbe2
kazewindser <kazewindser@gmail.com>
2025-12-19 19:11:18 +0900

    prompt构建完成

--- ElicitResponse/testdemo.py
+++ ElicitResponse/testdemo.py
@@ -45,34 +45,29 @@ class Prediction(BaseModel):
 ############# PROMPT ##################
 
     #"如果双方有提前chat交流，或者有进行trasnfer，就会有一定的概率被罚款（fine）"
+    SystemPROMPT_T3firstfine25 = (
+        "経済学実験における参加者間のチャットログを送信します。この実験は、2人（プレイヤー1とプレイヤー2）を1組とした「第一価格オークション（first price auction）」形式で行われます。両参加者は、自分だけが確認できる商品の「評価額（value）」を持っています。"
+        "入札前に両者はチャットでコミュニケートすることが可能です。入札後、最も高い価格（bid）を提示した者が勝者となり商品を受け取ります。勝者の利得（payoff）は「評価額 - 入札額（bid）」となります。また、勝者は自分の利得の一部を敗者に譲渡（transfer）することも選択できます。"
 
-SystemPROMPT = (
-    "我将发送给你一个经济学实验中的参与者之间的聊天记录；该经济学实验是一个两个人（player1 和player2）为一组的first price auction实验。 两个参与者都有各自的，只能自己看到的，对于拍卖的商品的value（評価額）,"
-    "在双方入札之前，双方可以选择进行进行chat交流；入札之后，bid价高者胜，获得商品，其收益为:評価額-出价bid；同时，胜者也可以选择将收益（payoff）中的一部分（transfer）分给败者"
-    "现在，请帮我分析聊天记录，并提取一些关键信息:"
-    "-player1是否告知自己的評価額（一个数字）：player1_whether_inform_exact_self_value"
-    "-player2是否告知自己的評価額（一个数字）：player2_whether_inform_exact_self_value"
-    "-player1是否告知自己的評価額的区间：player1_whether_inform_value_interval"
-    "-player2是否告知自己的評価額的区间：player2_whether_inform_value_interval"
-    "-如果player1有告知自己的評価額，那么这个評価額是多少 (如果没有的话则返回-1)：player1_inform_exact_self_value"
-    "-如果player2有告知自己的評価額，那么这个評価額是多少 (如果没有的话则返回-1)：player2_inform_exact_self_value"
-    "-如果player1有告知自己的評価額的区间，那么这个区间是多少(如果没有的话则返回-1)：player1_inform_value_interval_minbound, player1_inform_value_interval_maxbound"
-    "-如果player2有告知自己的評価額的区间，那么这个区间是多少(如果没有的话则返回-1)：player2_inform_value_interval_minbound, player2_inform_value_interval_maxbound"
-    "-player1是否告诉对方自己的之后的bid(一个数字)：player1_whether_inform_exact_self_bid"
-    "-player2是否告诉对方自己的之后的bid(一个数字)：player2_whether_inform_exact_self_bid"
-    "-player1是否告诉对方自己的之后的bid区间：player1_whether_inform_bid_interval"
-    "-player2是否告诉对方自己的之后的bid区间：player2_whether_inform_bid_interval"
-    "-如果player1有告诉对方自己的之后的bid，那么这个bid是多少(如果没有的话则返回-1)：player1_inform_exact_self_bid"
-    "-如果player2有告诉对方自己的之后的bid，那么这个bid是多少(如果没有的话则返回-1)：player2_inform_exact_self_bid"
-    "-注意：这里的bid是出价，而不是payoff（利得）"
-    "-如果player1有告诉对方自己的之后的bid区间，那么这个区间是多少(如果没有的话则返回-1)：player1_inform_bid_interval_minbound, player1_inform_bid_interval_maxbound"
-    "-如果player2有告诉对方自己的之后的bid区间，那么这个区间是多少(如果没有的话则返回-1)：player2_inform_bid_interval_minbound, player2_inform_bid_interval_maxbound"
-    "-双方是否提前决定winner(如果没有提前决定返回-1，如果决定是either one则返回0，如果决定是player1则返回1，如果决定是player2则返回2)：Whether_Pre_Decide_Winner"
-    "-当双方提前决定winner的时候，判断约定胜者给败者transfer的format, 返回following的五个选项之一，'50% of the payoff'/ 'Exact number' /'Interval'/'Some points (not specific)'/'Nothing(not mentioned or no pre-decided winner)': Pre_Decide_Winner_Committed_Transfer_Format"
-    "-胜者约定的给败者transfer的具体金额数字是多少(如果没有返回-1)：Pre_Decide_Winner_Committed_Transfer_Number"
-    "-胜者约定的给败者transfer的具体金额数字的区间是多少(如果没有返回-1)：Pre_Decide_Winner_Committed_Transfer_interval_minbound, Pre_Decide_Winner_Committed_Transfer_interval_maxbound"
-    "-判断是否双方有勾结(比如双方有提前决定winner，或者使用一些「共謀に誓います」、「協力します」等等可以表示双方有在勾结Collusion的象征或者象征)(如果没有返回-1，如果不确定则返回0，如果有勾结则返回1)：Whether_Collusion"
-)
+        "【罰金ルール】事前にチャットで交流したり譲渡（transfer）を行ったりした場合、談合（Collusion）とみなされ、両者に250ポイントの罰金が科される可能性があります。今回の談合検知確率は25％です。"
+        "- 談合が検知されなかった場合（確率75％）：罰金を支払う必要はありません。"
+        "- 談合が検知された場合（確率25％）：罰金250ポイントを支払う必要があります。"
+
+        "提供するチャットログを分析し、以下の情報を抽出してください："
+# ... (中略) ...
+        "- 談合（共謀・Collusion）の疑いがあるか判断してください（例：事前に勝者を決める、「共謀に誓います」「協力します」といった談合を示唆する発言があるなど）。（ない場合は-1、不明な場合は0、ある場合は1）：Whether_Collusion"
+    )
+
+    SystemPROMPT_T6secondfine25 = (
+        "経済学実験における参加者間のチャットログを送信します。この実験は、2人（プレイヤー1とプレイヤー2）を1組とした「第二価格オークション（second price auction）」形式で行われます。両参加者は、自分だけが確認できる商品の「評価額（value）」を持っています。"
+        "入札前に両者はチャットでコミュニケートすることが可能です。入札後、最も高い価格（bid）を提示した者が勝者となり商品を受け取ります。勝者の利得（payoff）は「評価額 - 2番目に高い入札額（敗者のbid）」となります。また、勝者は自分の利得の一部を敗者に譲渡（transfer）することも選択できます。"
+
+        "【罰金ルール】事前にチャットで交流したり譲渡（transfer）を行ったりした場合、談合（Collusion）とみなされ、両者に250ポイントの罰金が科される可能性があります。今回の談合検知確率は25％です。"
+        "- 談合が検知されなかった場合（確率75％）：罰金を支払う必要はありません。"
+        "- 談合が検知された場合（確率25％）：罰金250ポイントを支払う必要があります。"
+
+        "提供するチャットログを分析し、以下の情報を抽出してください："
+# ... existing code ...
 
 # 读取CSV文件
 df = pd.read_csv('logdemo.csv')
